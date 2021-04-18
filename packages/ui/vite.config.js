d0d06fa11f25803e319ba02ae240cfd4ddb4ed86
feugy <damien.feugas@gmail.com>
2021-04-18 09:49:06 +0200

    chore: simpler plugin name

--- packages/ui/vite.config.js
+++ packages/ui/vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import svelte from '@sveltejs/vite-plugin-svelte'
 import yaml from '@rollup/plugin-yaml'
-import atelier from '@atelier/vite-plugin-atelier-svelte'
+import atelier from '@atelier/vite-plugin-svelte'
 
 export default defineConfig({
   base: '', // allows embedded deployments
