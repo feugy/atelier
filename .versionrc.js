57770bec8422724a91243a922fda0f6cb83adeb1
feugy <damien.feugas@gmail.com>
2021-10-10 08:44:06 +0200

    fix(plugin-svelte): wrong UI version referenced chore: fixes standard-version updater

--- .versionrc.js
+++ .versionrc.js
@@ -1,9 +1,47 @@
+function isMelodieDep(name) {
+  return name?.startsWith('@atelier-wb') || name?.startsWith('atelier')
+}
+
+function updateDeps(package, version) {
+  if (isMelodieDep(package.name)) {
+    package.version = version
+  }
+  for (const deps of [
+    'dependencies',
+    'optionalDependencies',
+    'devDependencies',
+    'packages',
+    'requires'
+  ].filter(name => typeof package[name] === 'object')) {
+    for (const dep in package[deps]) {
+      if (deps === 'packages' || isMelodieDep(dep)) {
+        if (typeof package[deps][dep] === 'object') {
+          updateDeps(package[deps][dep], version)
+        } else {
+          package[deps][dep] = version
+        }
+      }
+    }
+  }
+  return package
+}
+
+const updater = {
+  readVersion(contents) {
+    return JSON.parse(contents).version
+  },
+  writeVersion(contents, version) {
+    return JSON.stringify(updateDeps(JSON.parse(contents), version))
+  }
+}
+
 module.exports = {
   bumpFiles: [
-    { filename: 'package.json', type: 'json' },
-    { filename: 'packages/plugin-svelte/package.json', type: 'json' },
-    { filename: 'packages/svelte/package.json', type: 'json' },
-    { filename: 'packages/toolshot/package.json', type: 'json' },
-    { filename: 'packages/ui/package.json', type: 'json' }
+    { filename: 'package.json', updater },
+    { filename: 'package-lock.json', updater },
+    { filename: 'packages/plugin-svelte/package.json', updater },
+    { filename: 'packages/svelte/package.json', updater },
+    { filename: 'packages/toolshot/package.json', updater },
+    { filename: 'packages/ui/package.json', updater }
   ]
 }
