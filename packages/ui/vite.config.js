83a633ea600a0864afb3cbcda5a0c8dad73ec02f
feugy <damien.feugas@gmail.com>
2022-09-08 23:23:42 +0200

    chore(ui): does not emit css during tests

--- packages/ui/vite.config.js
+++ packages/ui/vite.config.js
@@ -1,35 +1,43 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import yaml from '@rollup/plugin-yaml'
 import atelier from '@atelier-wb/vite-plugin-atelier'
+import sveltePreprocess from 'svelte-preprocess'
 import { defineConfig } from 'vite'
 import windi from 'vite-plugin-windicss'
 
-export default defineConfig({
-  base: '', // allows embedded deployments
-  plugins: [
-    windi(),
-    svelte(),
-    yaml(),
-    atelier({
-      path: './tests',
-      setupPath: './atelier-setup.js',
-      bundled: false
-    })
-  ],
-  build: {
-    rollupOptions: {
-      external: ['./ui-settings.js']
+export default defineConfig(({ mode }) => {
+  return {
+    base: '', // allows embedded deployments
+    plugins: [
+      windi({
+        transformCSS: 'pre'
+      }),
+      svelte({
+        emitCss: mode !== 'test',
+        preprocess: [sveltePreprocess()]
+      }),
+      yaml(),
+      atelier({
+        path: './tests',
+        setupPath: './atelier-setup.js',
+        bundled: false
+      })
+    ],
+    build: {
+      rollupOptions: {
+        external: ['./ui-settings.js']
+      }
+    },
+    server: {
+      port: 3001,
+      open: '/atelier',
+      fs: { strict: false }
+    },
+    test: {
+      deps: { fallbackCJS: true },
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: 'tests/setup'
     }
-  },
-  server: {
-    port: 3001,
-    open: '/atelier',
-    fs: { strict: false }
-  },
-  test: {
-    deps: { fallbackCJS: true },
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: 'tests/setup'
   }
 })
